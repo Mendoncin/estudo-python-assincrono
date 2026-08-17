b287a34d18cd2ab08de559484bcea4366dc79820
Gabriel Alencar <gabriel.cabecao.lol@gmail.com>
2026-08-17 15:19:16 -0300

    request sincrono

---
+++ sincrono.py
@@ -0,0 +1,27 @@
+import time
+import httpx
+
+from typing import Tuple
+
+Urls = Tuple[str]
+
+urls = ('https://www.google.com', ) * 50
+
+
+def send_request(count: int, url: str, client: httpx.Client):
+  print(f"sending request #{count}")
+  response = client.get(url)
+  print(f"got response for request {count}, status code: {response.status_code}")
+
+
+def main_sync(in_urls: Urls = urls):
+  with httpx.Client() as client:
+    for num, url in enumerate(in_urls):
+      send_request(num, url, client)
+
+
+if _name_ == "_main_":
+  start = time.perf_counter()
+  main_sync(urls)
+  sync_duration = time.perf_counter() - start
+  print(sync_duration)
