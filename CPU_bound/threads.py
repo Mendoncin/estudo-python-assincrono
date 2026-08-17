42748647b941c23fa899d5ed768af350cde099d3
Gabriel Alencar <gabriel.cabecao.lol@gmail.com>
2026-08-17 19:53:28 -0300

    thread fail cpu

---
+++ CPU_bound/threads.py
@@ -0,0 +1,55 @@
+import time
+from typing import Tuple, Sequence
+import threading
+
+SequenceInt = Sequence[int]
+TupleIntDivisors = Tuple[int]
+
+numbers = (
+    1_248_376,
+    3_517_920,
+    7_264_815,
+    9_831_442,
+    12_458_760,
+    18_327_645,
+    21_764_832,
+    27_915_360,
+)
+
+def factorize_single(number: int) -> TupleIntDivisors:
+    return tuple(x for x in range(1, number + 1) if number % x == 0)
+
+
+def factorize_single_print(index: int, number: int) -> None:
+    print(f"start task {index}")
+    dividers = factorize_single(number)
+    print(f"result of task {index}: factorize single({number} = {dividers})")
+    print("-" * 100)
+
+
+def main_threads(in_numbers: SequenceInt = numbers) -> None:
+    tasks = []
+    for index, number in enumerate(in_numbers):
+        tasks.append(
+            threading.Thread(
+                target=factorize_single_print,
+                args=(
+                    index,
+                    number
+                )
+            )
+        )
+        tasks[-1].start()
+
+    for task in tasks:
+        task.join()
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    main_threads()
+    thread_duration = time.perf_counter() - start
+    print(thread_duration)
+
+
+#mesmo com varais threads, por conta do GIL só um codigo python executa por vêz
