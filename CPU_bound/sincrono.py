a67b9bf56383c70ef0de379a9760d5030d111d0a
Gabriel Alencar <gabriel.cabecao.lol@gmail.com>
2026-08-17 18:51:15 -0300

    organizando pasta + cpu bound

---
+++ CPU_bound/sincrono.py
@@ -0,0 +1,38 @@
+import time
+from typing import Tuple, Sequence
+
+SequenceInt = Sequence[int]
+TupleIntDivisors = Tuple[int]
+
+numbers = (
+    1_248_376,
+    3_517_920,
+    7_264_815,
+    9_831_442,
+    12_458_760,
+    18_327_645,
+    21_764_832,
+    27_915_360,
+)
+
+def factorize_single(number: int) -> TupleIntDivisors:
+    return tuple(x for x in range(1, number + 1) if number % x == 0)
+
+
+def factorize_single_print(index: int, number: int) -> None:
+    print(f"start task {index}")
+    dividers = factorize_single(number)
+    print(f"result of task {index}: factorize single({number} = {dividers})")
+    print("-" * 100)
+
+
+def main_sync(in_numbers: SequenceInt = numbers) -> None:
+    for index, number in enumerate(in_numbers):
+        factorize_single_print(index, number)
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    main_sync()
+    sync_duration = time.perf_counter() - start
+    print(sync_duration)
